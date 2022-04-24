d10229f0710fce9506f9f1a921ef5fa20980e57b
sky0621 <sky14work@gmail.com>
2022-04-24 22:18:52 +0900

    continue

--- src/app/providers/with-query-client.tsx
+++ src/app/providers/with-query-client.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { QueryClientProvider } from 'react-query';
-import {queryClient} from "../api";
+import { queryClient } from '../api';
 
 const withQueryClient = (component: () => React.ReactNode) => (
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   <QueryClientProvider client={queryClient}>{component()}</QueryClientProvider>
 );
 
