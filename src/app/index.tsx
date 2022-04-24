90fb1406573da9704d970b8ce6faa4265bcfba08
sky0621 <sky14work@gmail.com>
2022-04-25 08:23:01 +0900

    continue

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,16 @@
 import './index.scss';
 import { Container } from '@mui/material';
-import React from 'react';
+import React, { Suspense } from 'react';
 import Menu from '../ui/pages/Menu';
 import Router from '../ui/pages/Router';
+import Loading from '../ui/atoms/Loading';
 
 const App = () => (
   <Container maxWidth="md">
-    <Menu />
-    <Router />
+    <Suspense fallback={<Loading message="sending ..." />}>
+      <Menu />
+      <Router />
+    </Suspense>
   </Container>
 );
 
