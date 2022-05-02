d0b253ef46044dd583714d305129c848ade59753
sky0621 <sky14work@gmail.com>
2022-05-02 11:34:07 +0900

    continue

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,27 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, StrictMode } from 'react';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { queryClient } from './api';
 import Router from '../ui/pages/Router';
 import Loading from '../ui/atoms/Loading';
 
 const App = () => (
-  <Suspense fallback={<Loading message="sending ..." />}>
-    <Router />
-  </Suspense>
+  <StrictMode>
+    <RecoilRoot>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <Suspense fallback={<Loading message="sending ..." />}>
+            <Router />
+          </Suspense>
+          {process.env.NODE_ENV === 'development' && (
+            <ReactQueryDevtools initialIsOpen={false} />
+          )}
+        </QueryClientProvider>
+      </BrowserRouter>
+    </RecoilRoot>
+  </StrictMode>
 );
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from 'app/index';
-import { QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import { BrowserRouter } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
-import { queryClient } from './app/api';
 
 const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <RecoilRoot>
-        <BrowserRouter>
-          <QueryClientProvider client={queryClient}>
-            <App />
-            {process.env.NODE_ENV === 'development' && (
-              <ReactQueryDevtools initialIsOpen={false} />
-            )}
-          </QueryClientProvider>
-        </BrowserRouter>
-      </RecoilRoot>
-    </React.StrictMode>,
-  );
+  root.render(<App />);
 }
