76c91c7767365b4e9709d19169ed8f2e8e966981
sky0621 <sky14work@gmail.com>
2022-05-03 21:57:06 +0900

    continue

--- src/ui/pages/Me/ui/Me.tsx
+++ src/ui/pages/Me/ui/Me.tsx
@@ -28,112 +28,164 @@ const Me: React.FC = () => {
         </Typography>
         {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
         <form onSubmit={handleSubmit(handleEditMe)}>
+          <Paper sx={{ my: { xs: 2, md: 4 }, p: { xs: 2, md: 3 } }}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
+                <Controller
+                  name="name"
+                  control={control}
+                  defaultValue={user?.name}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <TextField
+                      id="name"
+                      type="text"
+                      label="Name"
+                      required
+                      fullWidth
+                      autoComplete="given-name"
+                      variant="standard"
+                      {...field}
+                    />
+                  )}
+                />
+                {errors.name && <div>Name is required</div>}
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Controller
+                  name="username"
+                  control={control}
+                  defaultValue={user?.username}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <TextField
+                      id="username"
+                      type="text"
+                      label="User Name"
+                      required
+                      fullWidth
+                      autoComplete="given-name"
+                      variant="standard"
+                      {...field}
+                    />
+                  )}
+                />
+                {errors.username && <div>User Name is required</div>}
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Controller
+                  name="email"
+                  control={control}
+                  defaultValue={user?.email}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <TextField
+                      id="email"
+                      type="email"
+                      label="Email Address"
+                      required
+                      fullWidth
+                      autoComplete="email"
+                      variant="standard"
+                      {...field}
+                    />
+                  )}
+                />
+                {errors.email && <div>Email Address is required</div>}
+              </Grid>
+            </Grid>
+          </Paper>
+          <Paper sx={{ my: { xs: 2, md: 4 }, p: { xs: 2, md: 3 } }}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
+                <Controller
+                  name="address.street"
+                  control={control}
+                  defaultValue={user?.address?.street}
+                  render={({ field }) => (
+                    <TextField
+                      id="street"
+                      type="street"
+                      label="Street"
+                      fullWidth
+                      variant="standard"
+                      {...field}
+                    />
+                  )}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Controller
+                  name="address.suite"
+                  control={control}
+                  defaultValue={user?.address?.suite}
+                  render={({ field }) => (
+                    <TextField
+                      id="suite"
+                      type="suite"
+                      label="Suite"
+                      fullWidth
+                      variant="standard"
+                      {...field}
+                    />
+                  )}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="address2"
+                  name="address2"
+                  label="Address line 2"
+                  fullWidth
+                  autoComplete="shipping address-line2"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="city"
+                  name="city"
+                  label="City"
+                  fullWidth
+                  autoComplete="shipping address-level2"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="state"
+                  name="state"
+                  label="State/Province/Region"
+                  fullWidth
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="zip"
+                  name="zip"
+                  label="Zip / Postal code"
+                  fullWidth
+                  autoComplete="shipping postal-code"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="country"
+                  name="country"
+                  label="Country"
+                  fullWidth
+                  autoComplete="shipping country"
+                  variant="standard"
+                />
+              </Grid>
+            </Grid>
+          </Paper>
           <Grid container spacing={3}>
-            <Grid item xs={12} sm={6}>
-              <Controller
-                name="name"
-                control={control}
-                defaultValue={user?.name}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <TextField
-                    id="name"
-                    type="text"
-                    label="Name"
-                    required
-                    fullWidth
-                    autoComplete="given-name"
-                    variant="standard"
-                    {...field}
-                  />
-                )}
-              />
-              {errors.name && <div>Name is required</div>}
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Controller
-                name="username"
-                control={control}
-                defaultValue={user?.username}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <TextField
-                    id="username"
-                    type="text"
-                    label="User Name"
-                    required
-                    fullWidth
-                    autoComplete="given-name"
-                    variant="standard"
-                    {...field}
-                  />
-                )}
-              />
-              {errors.username && <div>User Name is required</div>}
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                id="address1"
-                name="address1"
-                label="Address line 1"
-                fullWidth
-                autoComplete="shipping address-line1"
-                variant="standard"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                id="address2"
-                name="address2"
-                label="Address line 2"
-                fullWidth
-                autoComplete="shipping address-line2"
-                variant="standard"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                id="city"
-                name="city"
-                label="City"
-                fullWidth
-                autoComplete="shipping address-level2"
-                variant="standard"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                id="state"
-                name="state"
-                label="State/Province/Region"
-                fullWidth
-                variant="standard"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                id="zip"
-                name="zip"
-                label="Zip / Postal code"
-                fullWidth
-                autoComplete="shipping postal-code"
-                variant="standard"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                id="country"
-                name="country"
-                label="Country"
-                fullWidth
-                autoComplete="shipping country"
-                variant="standard"
-              />
-            </Grid>
             <Grid item xs={12} md={6}>
               <TextField
                 required
