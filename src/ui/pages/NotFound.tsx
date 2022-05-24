1d5a92a57a4419e97311a1f85eb9ba30b717f7a3
sky0621 <sky14work@gmail.com>
2022-05-24 22:51:13 +0900

    continue

--- src/ui/pages/NotFound.tsx
+++ src/ui/pages/NotFound.tsx
@@ -5,11 +5,9 @@ import styles from './NotFound.module.scss';
 const NotFoundPage: React.FC = () => (
   <div className={styles.container}>
     <div className={styles.mainbox}>
-      <div className={styles.err}>4</div>
-      <i
-        className={`${styles.far} ${styles.faQuestionCircle} ${styles.faSpin}`}
-      />
-      <div className={styles.err2}>4</div>
+      <div className={styles.first}>4</div>
+      <div className={styles.second}>0</div>
+      <div className={styles.third}>4</div>
       <div className={styles.msg}>
         Maybe this page moved? Got deleted? Is hiding out in quarantine? Never
         existed in the first place?
